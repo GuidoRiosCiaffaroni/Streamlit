f0df1bdb59afa98f0ee9c2ae3cd7e7ece7318d10
Guido Rios Ciaffaroni <guidoriosciaffaroni@gmail.com>
2025-09-12 17:14:22 -0300

    2025_09_12_17_14_21

---
+++ entorno.sh
@@ -0,0 +1,10 @@
+mkdir ~/streamlit_app && cd ~/streamlit_app
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install streamlit
+
+streamlit --version
+
+streamlit run app.py
