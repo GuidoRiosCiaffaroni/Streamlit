39c6b0286906b092c5afa101f7a565659dcb4aaa
Guido Rios Ciaffaroni <guidoriosciaffaroni@gmail.com>
2025-09-19 07:13:36 -0300

    2025_09_19_07_13_36

--- Install_2024/02_entorno.sh
+++ Install_2024/02_entorno.sh
@@ -1,4 +1,4 @@
 #  Crear un entorno virtual (recomendado)
 sudo mkdir ~/streamlit_app && cd ~/streamlit_app
-sudo python3 -m venv venv
-sudo source venv/bin/activate
+# sudo python3 -m venv venv
+# sudo source venv/bin/activate
