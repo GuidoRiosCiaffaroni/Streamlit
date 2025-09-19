6acca9ca8873308229cd8242d292cc5744e61848
Guido Rios Ciaffaroni <guidoriosciaffaroni@gmail.com>
2025-09-19 05:21:58 -0300

    2025_09_19_05_21_58

---
+++ Install_2024/install.sh
@@ -0,0 +1,9 @@
+#1. Actualizar el sistema
+sudo apt update && sudo apt upgrade -y
+sudo apt install nano -y 
+sudo apt install tmux -y
+#2. Instalar dependencias basicas
+sudo apt install -y python3 python3-pip python3-venv
+
+python3 --version
+pip3 --version
