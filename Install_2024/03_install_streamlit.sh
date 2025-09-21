7ae531df8daecd0106a64078988453bfe61a4ace
Guido Rios Ciaffaroni <guidoriosciaffaroni@gmail.com>
2025-09-21 01:28:21 -0300

    2025_09_21_01_28_21

--- Install_2024/03_install_streamlit.sh
+++ Install_2024/03_install_streamlit.sh
@@ -1,2 +1,4 @@
 sudo pip install --upgrade pip
+python -m pip install --upgrade pip
 sudo pip install streamlit numpy pandas matplotlib seaborn plotly scikit-learn umap-learn kaleido openpyxl
+python -m pip install streamlit numpy pandas matplotlib seaborn plotly scikit-learn umap-learn kaleido openpyxl
